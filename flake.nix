1f459c2eb05b57767436100c7915591c35bd7bb7
yb <scauyb@qq.com>
2025-11-12 07:43:14 +0800

    feat(nvim): 初始化Neovim配置并集成到Home Manager
    - 添加flake.nix以管理Nix依赖- 配置Neovim作为默认编辑器- 包含gnumake、ripgrep和neovide工具
    - 设置nvim配置文件的递归链接- 启用Home Manager模块支持

--- flake.nix
+++ flake.nix
@@ -1,4 +1,27 @@
-# flake.nix
 {
-  description = "My NixOS configuration version 0.7 ";
+  description = "Standalone Neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    homeManagerModules.nvim-config = { pkgs, ... }: {
+      home.packages = with pkgs; [
+        gnumake
+        ripgrep
+        neovide
+      ];
+
+      xdg.configFile."nvim" = {
+        source = "${self}";
+        recursive = true;
+      };
+
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+      };
+    };
+  };
 }
